4dcd85777b50522409ddf93206a8262f2fe0548d
vir <virs98@outlook.com>
2023-11-23 17:56:45 +0800

    :sparkles: feat 上传组件

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ConfigProvider, theme } from "antd";
 import "./components/fontawesome/index";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ProConfigProvider } from "@ant-design/pro-components";
+import Upload from "./components/ProFrom/upload";
 
 function App() {
   const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -15,7 +17,17 @@ function App() {
           algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
         }}
       >
-        <RouterProvider router={router} />
+        <ProConfigProvider
+          valueTypeMap={{
+            upload: {
+              renderFormItem(text, props, dom) {
+                return <Upload value={text} {...props} />;
+              },
+            },
+          }}
+        >
+          <RouterProvider router={router} />
+        </ProConfigProvider>
       </ConfigProvider>
     </Provider>
   );
---
+++ src/components/ProFrom/upload/index.tsx
@@ -0,0 +1,103 @@
+import { getToken } from "@/utils/token";
+import { checkPath, getApiPrefix } from "@/utils/utils";
+import { ProFieldFCRenderProps } from "@ant-design/pro-components";
+import { UploadFile, Upload, Button } from "antd";
+import { v4 as uuid } from "uuid";
+import { FC, useEffect, useState } from "react";
+import { UploadChangeParam } from "antd/es/upload";
+import { UploadOutlined } from "@ant-design/icons";
+
+export interface UploadProps extends ProFieldFCRenderProps {
+  value?: any;
+}
+
+const MUpload: FC<UploadProps> = (props) => {
+  const { value, fieldProps } = props;
+  const {
+    dir = "default",
+    onChange: fOnChange,
+    value: fValue,
+    multiple = false,
+    maxCount,
+    ...rest
+  } = fieldProps ?? {};
+
+  const authorization = getToken() ?? "";
+
+  const [fileList, setFileList] = useState<Record<string, UploadFile[]>>({});
+
+  useEffect(() => {
+    const item: any = {};
+    if (!value) {
+      item[props.fieldProps.id] = [];
+    } else {
+      if (Array.isArray(value) && value.length !== 0) {
+        item[props.fieldProps.id] = value;
+      } else if (typeof value === "string") {
+        item[props.fieldProps.id] = [];
+      } else {
+        // PS:不出意外的话上传会走到这里,但是下边已经处理了所以这里不做处理
+      }
+    }
+    setFileList((pre: any) => ({ ...pre, ...item }));
+  }, [value]);
+
+  const onChange = (info: UploadChangeParam<UploadFile>) => {
+    let newFileList = [...info.fileList];
+    newFileList = newFileList.map((file) => {
+      if (file.response) {
+        file.url = file.response.url;
+      }
+
+      return file;
+    });
+    setFileList((pre: any) => ({
+      ...pre,
+      ...{
+        [fieldProps.id]: newFileList,
+      },
+    }));
+  };
+
+  const onRemove = (file: UploadFile) => {
+    const item: any = {};
+    item[fieldProps.id] = fileList[fieldProps.id].filter(
+      (f: any) => f.uid !== file.uid
+    );
+    setFileList((pre: any) => ({ ...pre, ...item }));
+  };
+
+  useEffect(() => {
+    if (Object.keys(fileList).length !== 0) {
+      const list = fileList[fieldProps.id];
+      if (maxCount > 1) {
+        fOnChange?.(list.map((i) => i.response));
+      } else {
+        fOnChange?.(list[0]?.response);
+      }
+    }
+  }, [fileList, maxCount]);
+
+  const uploadProps = {
+    action: getApiPrefix(`/resource${checkPath(dir)}`),
+    headers: {
+      authorization: `Bearer ${authorization}`,
+    },
+    fileList: fileList[fieldProps.id] ?? [],
+    maxCount,
+    onChange,
+    onRemove,
+    listType: "picture",
+    ...rest,
+  };
+
+  return (
+    <Upload {...uploadProps}>
+      {maxCount && fileList[fieldProps.id]?.length < maxCount && (
+        <Button icon={<UploadOutlined />}>上传</Button>
+      )}
+    </Upload>
+  );
+};
+
+export default MUpload;
