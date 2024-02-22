ffd718d9fb8dd09ef5ce4c52604deefa2f788f3c
vir <virs98@outlook.com>
2024-02-22 09:59:42 +0800

    :necktie: chore 增加登录页，注册页设置，登录页支持设置

--- src/views/auth/login.tsx
+++ src/views/auth/login.tsx
@@ -7,68 +7,77 @@ import { LoginForm, ProFormText } from "@ant-design/pro-components";
 import { LoginRequest } from "@/services/auth/interface";
 import { useRequest } from "ahooks";
 import { getPublicProject } from "@/services/system/project";
-import { Space } from "antd";
+import { Image, Space, Spin } from "antd";
 import { AuthPaths } from "./router";
 
 const LoginView = () => {
   const navigate = useNavigate();
 
-  const { data } = useRequest(getPublicProject);
+  const { data, loading } = useRequest(getPublicProject);
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center overflow-hidden">
-      <div className="bg-white rounded-lg shadow overflow-hidden">
-        <LoginForm<LoginRequest>
-          title={data?.name}
-          subTitle={data?.description}
-          onFinish={(values) => {
-            return new Promise((resolve) => {
-              postLogin(values)
-                .then((res) => {
-                  const { access_token, refresh_token, ...rest } = res;
-                  setUserInfo(rest);
-                  setToken(access_token);
-                  setRefreshToken(refresh_token);
-                  navigate(HomePaths.home);
-                  resolve(true);
-                })
-                .catch(() => {
-                  resolve(false);
-                });
-            });
-          }}
-        >
-          <ProFormText
-            name="email"
-            placeholder="邮箱"
-            rules={[
-              {
-                required: true,
-                message: "请输入邮箱",
-              },
-            ]}
-          />
-          <ProFormText.Password
-            name="password"
-            placeholder="密码"
-            rules={[
-              {
-                required: true,
-                message: "请输入密码",
-              },
-            ]}
-          />
-          <Space className="justify-between mb-6 w-full">
-            <a
-              onClick={() => {
-                navigate(AuthPaths.register);
-              }}
-            >
-              注册账号
-            </a>
-            <a href="">忘记密码</a>
-          </Space>
-        </LoginForm>
+    <div className="w-screen h-screen flex justify-center items-center overflow-hidden relative">
+      <div className="absolute left-0 right-0">
+        <Image
+          className="!w-screen !h-screen object-cover"
+          src={data?.login?.background?.url}
+          preview={false}
+        />
+      </div>
+      <div className="bg-white rounded-lg shadow overflow-hidden relative">
+        <Spin spinning={loading}>
+          <LoginForm<LoginRequest>
+            title={data?.login?.title}
+            subTitle={data?.login?.subTitle}
+            onFinish={(values) => {
+              return new Promise((resolve) => {
+                postLogin(values)
+                  .then((res) => {
+                    const { access_token, refresh_token, ...rest } = res;
+                    setUserInfo(rest);
+                    setToken(access_token);
+                    setRefreshToken(refresh_token);
+                    navigate(HomePaths.home);
+                    resolve(true);
+                  })
+                  .catch(() => {
+                    resolve(false);
+                  });
+              });
+            }}
+          >
+            <ProFormText
+              name="email"
+              placeholder="邮箱"
+              rules={[
+                {
+                  required: true,
+                  message: "请输入邮箱",
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              placeholder="密码"
+              rules={[
+                {
+                  required: true,
+                  message: "请输入密码",
+                },
+              ]}
+            />
+            <Space className="justify-between mb-6 w-full">
+              <a
+                onClick={() => {
+                  navigate(AuthPaths.register);
+                }}
+              >
+                注册账号
+              </a>
+              <a href="">忘记密码</a>
+            </Space>
+          </LoginForm>
+        </Spin>
       </div>
     </div>
   );
--- src/views/system/setting/index.tsx
+++ src/views/system/setting/index.tsx
@@ -1,13 +1,13 @@
+import BasePageContainer from "@/components/containter/base";
 import {
   ProjectData,
   addProject,
   getProject,
   updateProject,
 } from "@/services/system/project";
-import { baseFormItemLayout } from "@/utils/utils";
 import {
   BetaSchemaForm,
-  PageContainer,
+  ProCard,
   ProFormInstance,
 } from "@ant-design/pro-components";
 import { useRequest } from "ahooks";
@@ -28,63 +28,119 @@ const Setting = () => {
   }, [data]);
 
   return (
-    <PageContainer>
-      <BetaSchemaForm
-        {...baseFormItemLayout}
-        loading={loading}
-        formRef={ref}
-        onFinish={(values) => {
-          return new Promise((resolve) => {
-            (data?._id ? updateProject(data?._id, values) : addProject(values))
-              .then(() => {
-                message.success("保存成功");
-                resolve(true);
-                run({});
-              })
-              .catch(() => {
-                resolve(false);
+    <BasePageContainer>
+      <ProCard>
+        <div className="max-w-2xl mx-auto">
+          <BetaSchemaForm
+            loading={loading}
+            formRef={ref}
+            onFinish={(values) => {
+              return new Promise((resolve) => {
+                (data?._id
+                  ? updateProject(data?._id, values)
+                  : addProject(values)
+                )
+                  .then(() => {
+                    message.success("保存成功");
+                    resolve(true);
+                    run({});
+                  })
+                  .catch(() => {
+                    resolve(false);
+                  });
               });
-          });
-        }}
-        submitter={{
-          searchConfig: {
-            submitText: "保存",
-          },
-          render(props, dom) {
-            return (
-              <div className="flex items-center justify-center gap-2">
-                {...dom}
-              </div>
-            );
-          },
-        }}
-        columns={[
-          {
-            title: "系统名称",
-            dataIndex: "name",
-            valueType: "text",
-            formItemProps: {
-              rules: [
-                {
-                  required: true,
-                  message: "请输入名称",
+            }}
+            submitter={{
+              searchConfig: {
+                submitText: "保存",
+              },
+              render(props, dom) {
+                return (
+                  <div className="flex items-center justify-center gap-2">
+                    {...dom}
+                  </div>
+                );
+              },
+            }}
+            columns={[
+              {
+                title: "系统名称",
+                dataIndex: "name",
+                valueType: "text",
+                formItemProps: {
+                  rules: [
+                    {
+                      required: true,
+                      message: "请输入名称",
+                    },
+                  ],
                 },
-              ],
-            },
-          },
-          {
-            title: "描述",
-            dataIndex: "description",
-            valueType: "textarea",
-          },
-          {
-            title: "强制需要邀请码注册",
-            dataIndex: "forceInvitationCode",
-            valueType: "switch",
-          },
-        ]}
-      />
-    </PageContainer>
+              },
+              {
+                title: "描述",
+                dataIndex: "description",
+                valueType: "textarea",
+              },
+              {
+                title: "强制需要邀请码注册",
+                dataIndex: "forceInvitationCode",
+                valueType: "switch",
+              },
+              {
+                title: "登录页面设置",
+                valueType: "group",
+                columns: [
+                  {
+                    title: "标题",
+                    dataIndex: ["login", "title"],
+                    valueType: "text",
+                  },
+                  {
+                    title: "副标题",
+                    dataIndex: ["login", "subTitle"],
+                    valueType: "text",
+                  },
+                  {
+                    title: "背景",
+                    dataIndex: ["login", "background"],
+                    valueType: "upload",
+                    fieldProps: {
+                      maxCount: 1,
+                      accept: "image/*",
+                    },
+                  },
+                ],
+              },
+              {
+                title: "注册页面设置",
+                valueType: "group",
+                columns: [
+                  {
+                    title: "标题",
+                    dataIndex: ["register", "title"],
+                    valueType: "text",
+                  },
+                  {
+                    title: "副标题",
+                    dataIndex: ["register", "subTitle"],
+                    valueType: "text",
+                  },
+                  {
+                    title: "背景",
+                    dataIndex: ["register", "background"],
+                    valueType: "upload",
+                    fieldProps: {
+                      maxCount: 1,
+                      accept: "image/*",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+      </ProCard>
+    </BasePageContainer>
   );
 };
 
