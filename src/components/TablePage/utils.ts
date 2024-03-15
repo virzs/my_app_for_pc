dc01b22aebabb325f5b2799de7b0f17c14935db7
vir <virs98@outlook.com>
2024-03-15 14:10:55 +0800

    :technologist: chore table page 支持自动格式化日期

--- src/components/TablePage/utils.ts
+++ src/components/TablePage/utils.ts
@@ -1,3 +1,5 @@
+import { format, isValid, parseISO } from "date-fns";
+
 export const renderEmptyToBarre = (value: any) => {
   if (value instanceof Array) {
     if (value.length === 0) {
@@ -7,5 +9,7 @@ export const renderEmptyToBarre = (value: any) => {
   } else if (["", null, undefined].includes(value)) {
     return "-";
   }
-  return value;
+  return isValid(parseISO(value))
+    ? format(value, "yyyy-MM-dd HH:mm:ss")
+    : value;
 };
