c19a8cad3b216f2c3d4f97ce84233e1fb6b919bf
vir <virs98@outlook.com>
2024-03-21 17:54:08 +0800

    :necktie: chore 更新操作二次提示

--- src/views/system/role/index.tsx
+++ src/views/system/role/index.tsx
@@ -3,16 +3,13 @@ import { RoleRequest, deleteRole, getRole } from "@/services/system/role";
 import TablePage, { TablePageProps } from "@/components/TablePage";
 import TablePageContainer from "@/components/containter/table";
 import { ProCard } from "@ant-design/pro-components";
-import { Button, Modal, message } from "antd";
+import { Button, message } from "antd";
 import { useRequest } from "ahooks";
 import Operation from "@/components/TablePage/Operation";
 import { useNavigate } from "react-router-dom";
 import { SystemPaths } from "../router";
 
-const { useModal } = Modal;
-
 const Role = () => {
-  const [modal, contextHolder] = useModal();
   const navigate = useNavigate();
 
   const table = useTablePage<RoleRequest[]>(getRole);
@@ -77,25 +74,16 @@ const Role = () => {
             columns={[
               {
                 title: "修改",
-                onClick: (e) => {
-                  e.stopPropagation();
+                onClick: () => {
                   navigate(SystemPaths.roleHandle + "/" + _id);
                 },
               },
               {
                 title: "删除",
                 loading: delLoading,
-                onClick: (e) => {
-                  e.stopPropagation();
-                  modal.confirm({
-                    title: "确认删除?",
-                    content: "删除后不可恢复",
-                    onOk() {
-                      delRun(_id!);
-                    },
-                  });
-                },
+                onClick: () => delRun(_id!),
                 danger: true,
+                confirm: "delete",
               },
             ]}
           />
@@ -132,7 +120,6 @@ const Role = () => {
           },
         })}
       />
-      {contextHolder}
     </TablePageContainer>
   );
 };
