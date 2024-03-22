47fc50f59e4f7df137b14dd387f787fd698d633b
vir <virs98@outlook.com>
2024-03-22 15:16:17 +0800

    :bug: fix 文本编辑器回显

--- src/components/BlockNoteEditor/index.tsx
+++ src/components/BlockNoteEditor/index.tsx
@@ -3,11 +3,12 @@ import {
   DefaultBlockSchema,
   DefaultInlineContentSchema,
   DefaultStyleSchema,
+  BlockNoteEditor as BE,
 } from "@blocknote/core";
 import { resourceUpload } from "@/services/resource";
 import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { ProFieldFCRenderProps } from "@ant-design/pro-components";
 
 export interface BlockNoteEditorProps extends ProFieldFCRenderProps {
@@ -30,26 +31,32 @@ const BlockNoteEditor: FC<BlockNoteEditorProps> = (props) => {
 
   const { onChange: fOnChange, value: fValue } = fieldProps ?? {};
 
-  const editor = useCreateBlockNote({
-    initialContent: value ?? fValue,
-    uploadFile(file) {
-      return new Promise((resolve, reject) => {
-        resourceUpload("blocknote", file)
-          .then((res) => {
-            resolve(res.url);
-          })
-          .catch((e) => {
-            reject(e);
-          });
-      });
-    },
-  });
+  const ie = useCreateBlockNote();
+
+  const editor = useMemo(() => {
+    if (!value && !fValue) return;
+    return BE.create({
+      initialContent: value ?? fValue,
+      uploadFile(file) {
+        return new Promise((resolve, reject) => {
+          resourceUpload("blocknote", file)
+            .then((res) => {
+              resolve(res.url);
+            })
+            .catch((e) => {
+              reject(e);
+            });
+        });
+      },
+    });
+  }, [value, fValue]);
 
   return (
     <div className="border border-gray-200 border-solid p-2 rounded-lg h-96 overflow-y-auto">
       <BlockNoteView
-        editor={editor}
+        editor={editor ?? ie}
         onChange={() => {
+          if (!editor) return;
           onChange?.(editor.document);
           fOnChange?.(editor.document);
         }}
