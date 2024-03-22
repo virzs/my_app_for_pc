09552a54966cbdcdd9280e4cf1869368cbdbbac6
vir <virs98@outlook.com>
2024-03-22 15:01:13 +0800

    :bug: fix 文本编辑器适配新版本

--- src/components/BlockNoteEditor/index.tsx
+++ src/components/BlockNoteEditor/index.tsx
@@ -5,11 +5,12 @@ import {
   DefaultStyleSchema,
 } from "@blocknote/core";
 import { resourceUpload } from "@/services/resource";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 import { FC } from "react";
+import { ProFieldFCRenderProps } from "@ant-design/pro-components";
 
-export interface BlockNoteEditorProps {
+export interface BlockNoteEditorProps extends ProFieldFCRenderProps {
   value?: Block<
     DefaultBlockSchema,
     DefaultInlineContentSchema,
@@ -25,13 +26,12 @@ export interface BlockNoteEditorProps {
 }
 
 const BlockNoteEditor: FC<BlockNoteEditorProps> = (props) => {
-  const { value, onChange } = props;
+  const { value, onChange, fieldProps } = props;
 
-  const editor = useBlockNote({
-    initialContent: value,
-    onChange(editor) {
-      onChange?.(editor.topLevelBlocks);
-    },
+  const { onChange: fOnChange, value: fValue } = fieldProps ?? {};
+
+  const editor = useCreateBlockNote({
+    initialContent: value ?? fValue,
     uploadFile(file) {
       return new Promise((resolve, reject) => {
         resourceUpload("blocknote", file)
@@ -46,8 +46,14 @@ const BlockNoteEditor: FC<BlockNoteEditorProps> = (props) => {
   });
 
   return (
-    <div className="border border-gray-200 border-solid p-2 rounded-lg">
-      <BlockNoteView editor={editor} />
+    <div className="border border-gray-200 border-solid p-2 rounded-lg h-96 overflow-y-auto">
+      <BlockNoteView
+        editor={editor}
+        onChange={() => {
+          onChange?.(editor.document);
+          fOnChange?.(editor.document);
+        }}
+      />
     </div>
   );
 };
